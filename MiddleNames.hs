0ced419ee97121b5547d6f450a138244bec48b33
Ryan Mulligan <ryan@ryantm.com>
2015-11-05 08:14:44 -0800

    try2

--- MiddleNames.hs
+++ MiddleNames.hs
@@ -1,2 +1,44 @@
 module MiddleNames where
 
+import Data.Tree
+import Debug.Trace
+
+data Sex = Boy | Girl
+
+type Name = String
+
+type FamilyTree = Tree Name
+
+everyOther :: [a] -> [a]
+everyOther (x:y:xs) = [x] ++ everyOther xs
+everyOther [x] = [x]
+everyOther [] = []
+
+girlEveryOther l = everyOther (drop 1 l)
+
+everyOtherSplit :: [a] -> ([a],[a])
+everyOtherSplit (x:y:xs) = ([x] ++ fst (everyOtherSplit xs),
+                            [y] ++ snd (everyOtherSplit xs))
+everyOtherSplit [x] = ([x],[])
+everyOtherSplit [] = ([],[])
+
+reverseSnd :: ([a],[b]) ->  ([a],[b])
+reverseSnd (as,bs) = (as, reverse bs)
+
+
+allMiddleNames :: FamilyTree -> ([Name],[Name])
+allMiddleNames =
+  mconcat .
+  reverseGirls .
+  splitByGender .
+  grandparentsAndOlder .
+  sortByOldness
+  where
+    sortByOldness = levels
+    grandparentsAndOlder = drop 2
+    splitByGender = fmap everyOtherSplit
+    reverseGirls = fmap reverseSnd
+
+middleNames :: Sex -> FamilyTree -> [Name]
+middleNames Boy familyTree = fst (allMiddleNames familyTree)
+middleNames Girl familyTree = snd (allMiddleNames familyTree)
--- MiddleNamesSpec.hs
+++ MiddleNamesSpec.hs
@@ -2,4 +2,35 @@ module MiddleNamesSpec where
 
 import Test.Hspec
 
+import Data.Tree
+
 import MiddleNames
+
+
+familyTree = Node "Midas"
+             [Node "Ryan"
+              [Node "Kerry" [Node "Robert" [], Node "Claudine" []],
+               Node "Joan" [Node "Thomas" [], Node "Julia" []]],
+              Node "Andrea"
+              [Node "David" [Node "Carl" [], Node "Selma" []],
+               Node "Christine" [Node "John" [], Node "Anastasia" []]]]
+
+otherFamilyTree = Node "Alan"
+                  [Node "Paul"
+                   [Node "Daniel" [],
+                    Node "Carol" []],
+                   Node "Fang"
+                   [Node "Waiguo" [],
+                    Node "Waipo" []]]
+
+main = hspec (do
+  describe "middleNames" (do
+    it "should return expected names for boys" (do
+      middleNames Boy familyTree `shouldBe`
+        ["Kerry", "David", "Robert", "Thomas", "Carl", "John"])
+    it "should return expected names for girls" (do
+      middleNames Girl familyTree `shouldBe`
+        ["Christine", "Joan", "Anastasia", "Selma", "Julia", "Claudine"])
+    it "should return Daniel second for boys" (do
+      middleNames Boy otherFamilyTree !! 0 `shouldBe` "Daniel")
+                         ))
