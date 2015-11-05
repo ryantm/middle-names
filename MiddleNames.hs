e59cf8c48d599eea8b9f99bf8cb2e4a0fc688b56
Ryan Mulligan <ryan@ryantm.com>
2015-11-05 07:25:20 -0800

    clear out code

--- MiddleNames.hs
+++ MiddleNames.hs
@@ -1,24 +1,2 @@
 module MiddleNames where
 
-import Data.Tree
-
-data Sex = Boy | Girl
-
-type Name = String
-
-type FamilyTree = Tree Name
-
-name :: FamilyTree -> Name
-name = rootLabel
-
-reverseSnd :: ([a],[b]) -> ([a],[b])
-reverseSnd (as,bs) = (as,reverse bs)
-
-split :: [a] -> ([a], [a])
-split [] = ([],[])
-split (x:y:xs) = ([x] ++ fst (split xs), [y] ++ snd (split xs))
-
-ancestors ft = fmap reverseSnd (fmap split (drop 2 (levels ft)))
-
-middleNames Boy ft = fst (mconcat (ancestors ft))
-middleNames Girl ft = snd (mconcat (ancestors ft))
--- MiddleNamesSpec.hs
+++ MiddleNamesSpec.hs
@@ -1,35 +1,5 @@
 module MiddleNamesSpec where
 
-import Data.Tree
 import Test.Hspec
 
 import MiddleNames
-
-familyTree =
-  Node "Midas"
-   [Node "Ryan"
-    [Node "Kerry"
-     [Node "Grandpa Mulligan" [],
-      Node "Claudine" []],
-     Node "Joan"
-     [Node "Grandpa Egan" [],
-      Node "Julia" []]],
-    Node "Andrea"
-    [Node "David"
-     [Node "Carl" [],
-      Node "Selma" []],
-     Node "Christine"
-     [Node "Grandpa Christine" [],
-      Node "Nelly" []]]]
-
-main :: IO ()
-main = hspec (do
-  describe "middleNames" (do
-    it "the first boy should be Kerry" (do
-       head (middleNames Boy familyTree) `shouldBe` "Kerry")
-    it "the second boy should be David" (do
-       head (tail (middleNames Boy familyTree)) `shouldBe` "David")
-    it "the first girl should be Christine" (do
-       head (middleNames Girl familyTree) `shouldBe` "Christine")
-    it "the second girl should be Joan" (do
-       head (tail (middleNames Girl familyTree)) `shouldBe` "Joan")))
